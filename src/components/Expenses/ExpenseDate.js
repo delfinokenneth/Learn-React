95ac8ca9592b49ef6560c877e324bfb1ccb47dad
delfinokenneth <kenneth.delfino11@gmail.com>
2022-07-14 22:01:41 +0800

    udemy-react-guide-part52

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,4 +1,6 @@
-function ExpenseDate(props) {
+import './ExpenseDate.css';
+
+const ExpenseDate = (props) => {
     const month = props.date.toLocaleString('en-US', { month: 'long' });
     const day = props.date.toLocaleString('en-US', { day: '2-digit' });
     const year = props.date.getFullYear();
