b42603a8269d2611fd19830b54f4a788d13ae9f2
delfinokenneth <kenneth.delfino11@gmail.com>
2022-08-05 23:16:29 +0800

    udemy-react-guide-part91

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -6,16 +6,18 @@ const AddUser = (props) => {
     }
 
     return (
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username"> Username </label>
-            <input id="username" type="text"/>
+        <Card>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username"> Username </label>
+                <input id="username" type="text"/>
 
-            <label htmlFor="age"> Age (Years) </label>
-            <input id="age" type="number"/>
+                <label htmlFor="age"> Age (Years) </label>
+                <input id="age" type="number"/>
 
-            <button type="submit"> Add User</button>
+                <button type="submit"> Add User</button>
 
-        </form>
+            </form>
+        </Card>
     );
 };
 
