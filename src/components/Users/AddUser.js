7ccd767daad9c509880821f920f3a47e71e79d04
delfinokenneth <kenneth.delfino11@gmail.com>
2022-08-07 23:55:31 +0800

    udemy-react-guide-part91

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
 
 const AddUser = (props) => {
     const addUserHandler = (event) => {
@@ -6,7 +8,7 @@ const AddUser = (props) => {
     }
 
     return (
-        <Card>
+        <Card className={classes.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username"> Username </label>
                 <input id="username" type="text"/>
