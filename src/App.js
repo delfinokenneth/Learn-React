f33871297c3d4b90e73866f864af9b2f26d0f2e2
delfinokenneth <kenneth.delfino11@gmail.com>
2022-08-10 21:09:07 +0800

    udemy-react-guide-part97

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AddUser from './components/Users/AddUser';
-
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      console.log(prevUsersList);
+      console.log([...prevUsersList, { name: uName, age: uAge, id: Math.random().toString() }]);
+      console.log([...prevUsersList]);
+      return [...prevUsersList, { name: uName, age: uAge, id: Math.random().toString() }];
+    })
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList}/>
     </div>
   );
 }
