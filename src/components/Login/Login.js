e511724931adec9e4d8870a972adc0ed4e1d8cb0
delfinokenneth <kenneth.delfino11@gmail.com>
2022-09-16 23:45:04 +0800

    udemy-react-guide-part117

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -61,7 +61,7 @@ const Login = (props) => {
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(emailState.isValid);
+    dispatchEmail({type: 'INPUT_BLUR'});
   };
 
   const validatePasswordHandler = () => {
