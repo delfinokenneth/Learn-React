5938cdc9f9245a6d4c4016bdc8760bb9d31ffbb4
delfinokenneth <kenneth.delfino11@gmail.com>
2022-09-20 23:33:32 +0800

    udemy-react-guide-part118

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,10 +6,10 @@ import Button from '../UI/Button/Button';
 
 const emailReducer = (state, action) => {
   if (action.type === 'USER_INPUT') {
-  return {value: action.val, isValid: action.valid.includes('@')};
+  return {value: action.val, isValid: action.val.includes('@')};
   }
   if (action.type === 'INPUT_BLUR') {
-    return { value: '', isValid: false};
+    return { value: state.value, isValid: state.value.includes('@') };
   }
   return { value: '', isValid: false };
 };
@@ -23,7 +23,7 @@ const Login = (props) => {
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: '',
-    isValid: false,
+    isValid: null,
   });
 
   useEffect( () => {
