a842d66748f1a49ade2b3449de5bc296b778980d
delfinokenneth <kenneth.delfino11@gmail.com>
2022-09-23 23:17:33 +0800

    udemy-react-guide-part118

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -14,6 +14,16 @@ const emailReducer = (state, action) => {
   return { value: '', isValid: false };
 };
 
+const passwordReducer = (state,action) => {
+  if (action.type === 'USER_INPUT') {
+    return {value: action.val, isValid: action.val.includes('@')};
+    }
+    if (action.type === 'INPUT_BLUR') {
+      return { value: state.value, isValid: state.value.includes('@') };
+    }
+    return { value: '', isValid: false };
+}
+
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState('');
   // const [emailIsValid, setEmailIsValid] = useState();
