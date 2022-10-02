9328c034055c423ee867fb158999c07e384e6224
delfinokenneth <kenneth.delfino11@gmail.com>
2022-10-02 23:56:07 +0800

    udemy-react-guide-part120

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -50,11 +50,14 @@ const Login = (props) => {
     };
   }, []);
 
+  const { isValid: emailIsValid } = emailState;
+  const { isValid: passwordIsValid} = passwordState;
+
   useEffect(() => {
     const identifier = setTimeout( () => {
       console.log('Checking form validity');
       setFormIsValid(
-        emailState.isValid && passwordState.isValid 
+        emailIsValid && passwordIsValid 
       );
     }, 500);
     clearTimeout();
@@ -63,7 +66,7 @@ const Login = (props) => {
       console.log('CLEANUP');
       clearTimeout(identifier);
     };
-  }, [emailState, passwordState]);
+  }, [emailIsValid, passwordIsValid]);
 
   const emailChangeHandler = (event) => {
     dispatchEmail({type: 'USER_INPUT', val: event.target.value});
